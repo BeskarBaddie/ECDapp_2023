e0d58fac8851a9627501e2453e9529d9641b5ee9
Rinya <rinyasingh@gmail.com>
2023-07-27 14:55:42 +0200

    analytics on dashboard

--- app/src/main/java/com/honours/ecd_2023/Dashboard.java
+++ app/src/main/java/com/honours/ecd_2023/Dashboard.java
@@ -7,9 +7,12 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 
 public class Dashboard extends AppCompatActivity {
@@ -17,13 +20,14 @@ public class Dashboard extends AppCompatActivity {
 
 
     CardView videoCard;
-
+    private FirebaseAnalytics mFirebaseAnalytics;
     CardView articlesCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         articlesCard = findViewById(R.id.cardArticle);
         videoCard = findViewById(R.id.cardContent);
 
@@ -32,7 +36,11 @@ public class Dashboard extends AppCompatActivity {
             public void onClick(View v) {
 
                 goToVideoContent();
-
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "video_card");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "video_button");
+                Log.d("FirebaseEvent", "Article card clicked" + bundle.toString());
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
             }
         });
 
@@ -40,6 +48,11 @@ public class Dashboard extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 goToArticles();
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "articles_card");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "articles_button");
+                Log.d("FirebaseEvent", "Video card clicked" + bundle.toString());
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
             }
         });
 
