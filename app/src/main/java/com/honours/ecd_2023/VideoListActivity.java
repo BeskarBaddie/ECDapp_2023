2b63a59ededfa9ac09f7d67a0f4c3b7149a13404
Tariro <88428902+BeskarBaddie@users.noreply.github.com>
2023-08-03 21:26:37 +0200

    Downloading a video saves it in local storage and from local storage you can play all your downloaded videos

---
+++ app/src/main/java/com/honours/ecd_2023/VideoListActivity.java
@@ -0,0 +1,67 @@
+package com.honours.ecd_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoListActivity extends AppCompatActivity implements VideoListAdapter.OnItemClickListener {
+    private RecyclerView recyclerView;
+    private VideoListAdapter adapter;
+    private List<File> videoFilesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_list);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Get the list of video files from the internal storage
+        videoFilesList = getVideoFilesFromInternalStorage();
+
+        // Initialize and set up the RecyclerView adapter
+        adapter = new VideoListAdapter(this, videoFilesList);
+        adapter.setOnItemClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Method to retrieve video files from internal storage
+    private List<File> getVideoFilesFromInternalStorage() {
+        List<File> videoFiles = new ArrayList<>();
+        File internalStorageDir = getApplicationContext().getFilesDir();
+        File[] files = internalStorageDir.listFiles();
+
+        // Add all video files to the list
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".mp4")) {
+                    videoFiles.add(file);
+                }
+            }
+        }
+        return videoFiles;
+    }
+
+    // RecyclerView item click event handler
+    @Override
+    public void onItemClick(int position) {
+        // Handle item click here
+        // When an item is clicked, open the FullscreenVideo activity and pass the selected video's URI to it.
+        File selectedVideoFile = videoFilesList.get(position);
+        Uri videoUri = Uri.fromFile(selectedVideoFile);
+
+        Intent intent = new Intent(this, FullscreenVideo.class);
+        intent.putExtra("ur", videoUri.toString());
+        intent.putExtra("nm", selectedVideoFile.getName());
+        startActivity(intent);
+    }
+}
